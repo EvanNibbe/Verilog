8e8e33337ee31ece02ebea245d89a4e4eab455ee
Eddie Carrizales <68273063+Eddie-Carrizales@users.noreply.github.com>
2021-09-17 17:56:41 -0500

    Update FinalPart1.v

--- FinalPart1.v
+++ FinalPart1.v
@@ -95,37 +95,3 @@ module testbench();
   end  //End the code block of the main (initial)
   
 endmodule //Close the testbench module
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Dr. Becker's cheat sheet of what is wrong in the code.
-//The loop control variable can never reach 16, it is only 4 bits. Add another bit
-//There needs to be a time dealy, such a #5, inside the for loop
